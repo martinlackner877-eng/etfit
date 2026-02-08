d0446b6b3c0804a1fce162049f4f009a558a1429
Martin Lackner <martinlac@edu.aau.at>
2026-02-08 23:47:43 +0100

    contact

--- src/app/pages/contact/contact.ts
+++ src/app/pages/contact/contact.ts
@@ -39,23 +39,26 @@ export class ContactPageComponent {
       }
     });
 
+
+
     try {
       const response = await fetch('/', {
         method: 'POST',
         headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
         body: body.toString()
       });
-
+  console.log('Response Status:', response.status); // DEBUG
+  console.log('Response:', response); // DEBUG
       if (response.ok) {
         this.formSuccess = true;
         form.reset();
       } else {
         this.formError = true;
       }
-    } catch {
+    } catch (error) {
+      console.error('Fetch Error:', error); // DEBUG
       this.formError = true;
-    } finally {
-      this.isSubmitting = false;
+
     }
   }
 }
