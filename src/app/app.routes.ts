27a0ff566528cfd34a95f552a202114774e9190d
Martin Lackner <martinlac@edu.aau.at>
2026-02-09 22:20:09 +0100

    404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,11 @@ import { Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home/home';
 import { ContactPageComponent } from './pages/contact/contact';
 import { ThankYouComponent } from './pages/thank-you/thank-you';
-
+import { NotFoundComponent } from './pages/not-found/not-found';
 
 export const routes: Routes = [
 	{ path: '', component: HomePageComponent },
 	{ path: 'contact', component: ContactPageComponent },
 	{ path: 'thank-you', component: ThankYouComponent },
-	{ path: '**', redirectTo: '' }
+	{ path: '**', component: NotFoundComponent }
 ];
