e20a9c8c71cf812c1c8ed617369bd27f62df3932
Martin Lackner <martinlac@edu.aau.at>
2026-02-09 21:59:30 +0100

    feat: add access granted page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home/home';
 import { ContactPageComponent } from './pages/contact/contact';
+import { ThankYouComponent } from './pages/thank-you/thank-you';
+
 
 export const routes: Routes = [
 	{ path: '', component: HomePageComponent },
 	{ path: 'contact', component: ContactPageComponent },
+	{ path: 'thank-you', component: ThankYouComponent },
 	{ path: '**', redirectTo: '' }
 ];
